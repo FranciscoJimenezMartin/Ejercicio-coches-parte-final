ae9443c321e3352544ee4577d0da78693d393258
Kisko <Kisko@Kisko-PC>
2017-03-26 19:51:19 +0300

    Segunda subida del proyecto

--- src/ejercicio3ed/Camion.java
+++ src/ejercicio3ed/Camion.java
@@ -9,6 +9,52 @@ package ejercicio3ed;
  *
  * @author kisko
  */
-public class Camion {
+public class Camion extends VehiculoCarga
+{
+    /**
+     * Constructor por defecto
+     */
+    public Camion() 
+    {
+        super();
+    }
+    
+    /**
+     * Contructor por parametros
+     * @param matricula
+     * @param duracion_alquiler
+     * @param n_plazas
+     * @param carga_maxima 
+     */
+
+    public Camion(String matricula, int duracion_alquiler, int n_plazas, double carga_maxima) {
+        super(matricula, duracion_alquiler, n_plazas, carga_maxima);
+    }
+    
+    /**
+     * Metodo alquiler para saber lo que hay que pagar
+     * @return 
+     */
+    @Override
+    public double Alquiler ()
+    {
+        double importe_alquiler = 0;
+        importe_alquiler = (50 * super.getDuracion_alquiler() + 20 * super.getCarga_maxima() +40 );
+        return importe_alquiler;
+    }
+    
+    /**
+     * Metodo toString con el recibo
+     */
+    @Override
+    public void Recibo ()
+    {
+        System.out.println("Matrícula: " + super.getMatricula() + "\n" + "Duración: " + super.getDuracion_alquiler() + "\n" + "Tara:: " + super.getCarga_maxima() + "\n" + "Importe: " + this.Alquiler()+ "€");
+    }
+
+    
+    
+    
+  
     
 }
